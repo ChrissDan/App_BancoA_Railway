8ab45423f766fc24a80acacdde49ef8ac785a8ec
ChrissDan <105939064+ChrissDan@users.noreply.github.com>
2024-07-01 10:53:04 -0500

    Add files via upload

---
+++ src/main/java/com/sistemasdistribuidos/banco/bakend/API_REST_BANCO_BACKEND/ApiRestBancoBackendApplication.java
@@ -0,0 +1,34 @@
+package com.sistemasdistribuidos.banco.bakend.API_REST_BANCO_BACKEND;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class ApiRestBancoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestBancoBackendApplication.class, args);
+	}
+
+	@Configuration
+	public static class WebConfig {
+
+		@Bean
+		public WebMvcConfigurer corsConfigurer() {
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
+							.allowedHeaders("*")
+							.allowCredentials(true);
+				}
+			};
+		}
+	}
+
+}
